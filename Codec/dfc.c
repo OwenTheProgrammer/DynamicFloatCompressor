a5c1b78093dcec41714d906f5acf1f1a0524a309
Owen Cook <owenmckenziecookbusiness@gmail.com>
2023-01-10 22:10:27 -0500

    Version 1.0 finally complete.

---
+++ Codec/dfc.c
@@ -0,0 +1,230 @@
+#include "dfc.h"
+
+//Public functions
+dfc_value_t dfc_cvtps_df(const float value, 
+						 const float maxErr, 
+						 const float zeroThresh) {
+	//Zero encoding
+	if(dfc_abs_ps(value) <= zeroThresh) {
+		return (dfc_value_t){0, DFC_ZERO_LITERAL};
+	}
+
+	dfc_floatMem_t binding = (dfc_floatMem_t){.single = value};
+	//NaN and inf encoding
+	if(binding.exp == 255) {
+		uint8_t tag = binding.frac ? DFC_NAN_LITERAL : DFC_INF_LITERAL;
+		return (dfc_value_t){0, tag};
+	}
+
+	//Find best fraction/mantissa
+	dfc_value_t encoded = dfc_nomm_encodeFrac(binding, maxErr);
+	
+	//Encoded as boundary
+	if(encoded.tag.value == 0xFF) {
+		encoded.tag.value = 0;
+		binding.exp += encoded.memory;
+	}
+
+	//Pack exponent
+	int exponentLength;
+	uint8_t packedExponent = dfc_packExponent(binding.exp, &exponentLength);
+	encoded.tag.exp = (uint8_t)exponentLength;
+
+	//Save exponent with its signature
+	encoded.memory <<= exponentLength + 1;
+	encoded.memory |= packedExponent;
+
+	//Save negative sign
+	encoded.memory <<= 1;
+	encoded.memory |= binding.sign;
+	return encoded;
+}
+float dfc_cvtdf_ps(const dfc_value_t value) {
+	dfc_floatMem_t buffer;
+	switch(value.tag.value) {
+		case DFC_ZERO_LITERAL:
+			return 0.0f;
+		case DFC_INF_LITERAL:
+			buffer.memory = 0x7F800000;
+			return buffer.single;
+		case DFC_NAN_LITERAL:
+			buffer.memory = 0x7FFFFFFF;
+			return buffer.single;
+		default: //2^0
+			buffer.memory = 0x3F800000;
+			break;
+	}
+
+	//Decode the sign of the float
+	uint32_t memory = value.memory;
+	buffer.sign = memory & 1;
+	memory >>= 1;
+
+	//Decode the exponent
+	if(value.tag.exp) {
+		buffer.exp = dfc_unpackExponent(memory, value.tag.exp);
+		memory >>= value.tag.exp + 1;
+	}
+
+	//value has 23 real bits as the float frac
+	if((value.tag.value & DFC_F32_LITERAL) == DFC_F32_LITERAL) {
+		buffer.frac = memory;
+		return buffer.single;
+	}
+
+	//Construct real part
+	uint8_t realOffset = 23 - value.tag.real;
+	uint32_t realMask = 0xFF800000 >> value.tag.real;
+	buffer.frac = memory << realOffset;
+	buffer.frac &= realMask;
+
+	//Construct run part
+	memory &= dfc_bitMask(value.tag.run * 4);
+	memory *= dfc_mulTable[value.tag.run];
+	memory >>= (value.tag.real + 1);
+	buffer.frac |= memory & (~realMask);
+	
+	return buffer.single;
+}
+int dfc_getEncodedLength(const dfc_tag_t tag) {
+	switch (tag.value) {
+		case DFC_ZERO_LITERAL:
+		case DFC_INF_LITERAL:
+		case DFC_NAN_LITERAL:
+			return 0;
+	}
+	//Init with exponent length if its not zero
+	int size = (tag.exp + 1) * (tag.exp != 0);
+	//if encoding is a float, 23 real else encoded size
+	if((tag.value & DFC_F32_LITERAL) == DFC_F32_LITERAL) {
+		size += 23;
+	} else {
+		size += tag.run * 4 + tag.real;
+	}
+	return size;
+}
+
+//Compiler specific implementations
+inline void dfc_bitScanReverse(int* const pos, const int value) {
+#if defined(DFC_COMPILE_MSVC)
+	_BitScanReverse((unsigned long*)pos, value);
+#elif defined(DFC_COMPILE_GCC)
+	*pos = 31 - __builtin_clz(value);
+#else
+	int c = 1<<31;
+	*pos = 31;
+	while(1) {
+		if(value & c)
+			break;
+		c >>= 1;
+		(*pos)--;
+	}
+#endif
+}
+
+//Private functions
+uint8_t dfc_packExponent(uint8_t exponent, int* const length) {
+	exponent -= 127;
+	if(exponent == 0) {
+		*length = 0;
+		return 0;
+	}
+	uint8_t sign = exponent >> 7;
+	if(sign) exponent = -exponent;
+	dfc_bitScanReverse(length, exponent, 7);
+	(*length)++;
+	exponent |= sign << (*length);
+	return exponent;
+}
+uint8_t dfc_unpackExponent(uint8_t value, const uint8_t length) {
+	if(length == 0) return 127u;
+	uint8_t mask = dfc_bitMask(length);
+	uint8_t sign = value >> length;
+	value &= mask;
+	if(sign) value = -value;
+	return 127+value;
+}
+
+inline float dfc_sad_ps(const float a, const float b) {
+	//float diff = a - b;
+	//uint32_t mem = *(uint32_t*)&diff;
+	//mem &= 0x7FFFFFFF;
+	//return *(float*)&mem;
+	return fabsf(a - b);
+}
+inline float dfc_abs_ps(const float value) {
+	//uint32_t mem = *(uint32_t*)&value;
+	//mem &= 0x7FFFFFFF;
+	//return *(float*)&mem;
+	return fabsf(value);
+}
+
+dfc_value_t dfc_nomm_encodeFrac(const dfc_floatMem_t value, const float maxErr) {
+	uint32_t baseFrac = value.frac;
+
+	uint32_t mask = 0xFF800000;
+	uint32_t valueTemplate = value.memory & mask;
+	
+	//Test the boundary exponents
+	dfc_floatMem_t bound;
+	bound.memory = valueTemplate;
+	bound.exp++;
+	if(dfc_sad_ps(bound.single, value.single) <= maxErr)
+		return (dfc_value_t){1, 0xFF};
+	bound.exp -= 2;
+	if(dfc_sad_ps(bound.single, value.single) <= maxErr)
+		return (dfc_value_t){(-1), 0xFF};
+
+	for(int i = 0; i < dfc_length(dfc_tagTable) - 1; i++) {
+		dfc_tag_t tag = (dfc_tag_t){.value = dfc_tagTable[i]};
+
+		//Construct the real part
+		int realOffset = 23 - tag.real;
+		uint32_t realMask = (mask >> tag.real);
+
+		uint32_t evalBuffer = baseFrac & realMask;
+		uint32_t encodeBuffer = baseFrac >> realOffset;
+	
+		//Constructing the run can be expensive
+		if(tag.run) {
+			int runBits = tag.run * 4;
+			int runOffset = (23 - (runBits + tag.real));
+			uint32_t runValue = baseFrac >> runOffset;
+			runValue &= dfc_bitMask(runBits);
+			encodeBuffer |= (runValue << tag.real);
+
+			runValue *= dfc_mulTable[tag.run];
+			runValue >>= (tag.real + 1);
+			evalBuffer |= runValue & (~realMask);
+		}
+		//Evaluate the encoded values error
+		evalBuffer |= valueTemplate;
+		dfc_floatMem_t encoded = (dfc_floatMem_t){.memory = evalBuffer};
+		float error = dfc_sad_ps(encoded.single, value.single);
+
+		//Flip the last real bit (if used) and redo evaluation
+		if(tag.real) {
+			encoded.memory ^= (1 << realOffset);
+			float flip = dfc_sad_ps(encoded.single, value.single);
+
+			//update buffer if the flip was better
+			if(flip < error) {
+				encodeBuffer ^= 1;
+				error = flip;
+			}
+		}
+
+		//If encoding was suitable within maxErr
+		if(error <= maxErr) {
+			dfc_value_t df;
+			df.memory = encodeBuffer;
+			df.tag = tag;
+			return df;
+		}
+	}
+	//Fallback F32
+	dfc_value_t df;
+	df.memory = value.frac;
+	df.tag.value = DFC_F32_LITERAL;
+	return df;
+}
---
+++ Codec/dfc.h
@@ -0,0 +1,160 @@
+#pragma once
+#include <stdint.h>
+#include <math.h> //fabsf only
+
+//Windows header removal
+#if defined(WIN32)||defined(_WIN32)||defined(__WIN32)&&!defined(__CYGWIN__)
+#define VC_EXTRALEAN
+#define WIN32_LEAN_AND_MEAN
+#endif
+
+#if !defined(DFC_BUILD_STATIC)
+//DLL export defines
+	#if defined(DFC_BUILD_DLL)
+	#define DFC_API __declspec(dllexport)
+	#else
+	#define DFC_API __declspec(dllimport)
+	#endif
+#else
+//Static export defines
+	#if defined(__cplusplus)
+	#define DFC_API
+	#else
+	#define DFC_API extern
+	#endif
+#endif
+
+#if defined(_MSC_VER)
+#define DFC_COMPILE_MSVC
+#elif (defined(__GNUC__) && !defined(__clang__))
+#define DFC_COMPILE_GCC
+#endif
+
+#if defined(DFC_COMPILE_MSVC)
+#include <intrin.h>
+#endif
+
+#if defined(__cplusplus)
+namespace dfc {
+#endif
+
+#define dfc_length(arr) (sizeof(arr)/sizeof(*arr))
+#define dfc_bitMask(bits) ((1<<(bits))-1)
+
+#define DFC_F32_LITERAL 0x1F
+#define DFC_ZERO_LITERAL 0xFC
+#define DFC_INF_LITERAL 0xFD
+#define DFC_NAN_LITERAL 0xFE
+
+#if defined(__cplusplus)
+extern "C" {
+#endif
+
+static const uint16_t dfc_tagTable[32] = {
+	0,  4,  8,  12, 16, 1,  20, 5,
+	24, 9,  28, 13, 2,  17, 21, 6,
+	25, 10, 29, 14, 3,  18, 22, 7,
+	26, 11, 30, 15, 19, 23, 27, 31
+};
+static const int dfc_mulTable[4] = {
+	0, 0x111111, 0x10101, 0x1001
+};
+
+typedef union dfc_floatMem {
+	float single;
+	uint32_t memory;
+	struct {
+		uint32_t frac : 23;
+		uint32_t exp : 8;
+		uint32_t sign : 1;
+	};
+} dfc_floatMem_t;
+
+typedef union dfc_tag {
+	uint8_t value;
+	struct {
+		uint8_t run : 2;
+		uint8_t real : 3;
+		uint8_t exp : 3;
+	};
+} dfc_tag_t;
+
+typedef struct dfc_value {
+	uint32_t memory;
+	dfc_tag_t tag;
+} dfc_value_t;
+
+//Public Functions
+
+/// <summary>
+/// Encodes a float to a dynamic float.
+/// </summary>
+/// <param name="value">: The input float</param>
+/// <param name="maxErr">: Max encoding precision error +-</param>
+/// <param name="zeroThresh">: The threshold to truncate encoding to zero</param>
+/// <returns>The encoded DFC value</returns>
+DFC_API dfc_value_t dfc_cvtps_df(const float value, 
+								 const float maxErr, 
+								 const float zeroThresh);
+/// <summary>
+/// Decodes a dynamic float to a single precision float.
+/// </summary>
+/// <param name="value">: The encoded dfc value</param>
+/// <returns>The decoded float</returns>
+DFC_API float dfc_cvtdf_ps(const dfc_value_t value);
+/// <summary>
+/// Gets the length of the encoded dfc buffer.
+/// </summary>
+/// <param name="tag">: The encoder tag</param>
+/// <returns>the length in bits</returns>
+DFC_API int dfc_getEncodedLength(const dfc_tag_t tag);
+
+
+//Compiler specific implementations
+inline void dfc_bitScanReverse(int* const pos, const int value);
+
+//Private functions
+
+/// <summary>
+/// Packs an exponent value into its smallest binary form.
+/// </summary>
+/// <param name="exponent">: The float exponent</param>
+/// <param name="length">: The length in bits (output)</param>
+/// <returns>The encoded buffer</returns>
+uint8_t dfc_packExponent(uint8_t exponent, int* const length);
+/// <summary>
+/// Unpacks an encoded exponent to a float compliant exponent.
+/// </summary>
+/// <param name="value">: The encoded buffer</param>
+/// <param name="length">: The bit length of the buffer</param>
+/// <returns>A float exponent</returns>
+uint8_t dfc_unpackExponent(uint8_t value, const uint8_t length);
+
+/// <summary>
+/// Returns the absolute difference between <paramref name="a"/> and <paramref name="b"/>
+/// <remarks>sad : "Sum of Absolute Differences"</remarks>
+/// </summary>
+/// <param name="a">: First float argument</param>
+/// <param name="b">: Second float argument</param>
+/// <returns>Absolute difference</returns>
+inline float dfc_sad_ps(const float a, const float b);
+/// <summary>
+/// The absolute of a float.
+/// </summary>
+/// <param name="value"></param>
+/// <returns></returns>
+inline float dfc_abs_ps(const float value);
+
+/// <summary>
+/// Evaluates the best float fraction storage method
+/// </summary>
+/// <remarks> nomm: no SIMD </remarks>
+/// <param name="value">: The input float binding</param>
+/// <param name="maxErr">: The max error +-</param>
+/// <returns>The fraction encoding</returns>
+dfc_value_t dfc_nomm_encodeFrac(const dfc_floatMem_t value, const float maxErr);
+
+#if defined(__cplusplus)
+} //extern "C"
+} //namespace
+#endif
